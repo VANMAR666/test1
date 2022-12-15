a7d0d99771643fda5e6694963a1ad5e6cc9c8e89
semyonkungurov <s.kungurov8679@mail.ru>
2022-12-15 16:01:29 +0700

    create slider

---
+++ test.js
@@ -0,0 +1,11 @@
+var showPrefBtn=document.getElementById('show-pref-btn');
+var showNextBtn=document.getElementById('show-next-btn');
+showPrefBtn.addEventListener('click',prefSlider);
+showNextBtn.addEventListener('click',nextSlider);
+
+function prefSlider(){
+    console.log('<')
+}
+function nextSlider(){
+    console.log('>')
+}
