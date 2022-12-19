ee2eabe6c36e4fe3742db9f50da9b71e2e685143
semyonkungurov <s.kungurov8679@mail.ru>
2022-12-19 14:26:20 +0700

    constructor implemented

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-slider1 = sliderFactory.createNewSlider();
-slider2 = sliderFactory.createNewSlider();
+slider1 = new Slider();
+slider2 = new Slider();
 slider1.start('js-slider1');
 slider2.start('js-slider2');
